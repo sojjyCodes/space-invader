20b675ed99db6eabdcbc039ca9cf4a15ee7284c6
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-07-08 16:00:38 +0100

    Tweaked some things

--- space.py
+++ space.py
@@ -6,7 +6,7 @@ wn.bgcolor("red")
 wn.title("sojjy's Space invaders")
 #Draw border
 border_pen = turtle.Turtle() 
-border_pen.speed(1)
+border_pen.speed(0)
 border_pen.color("white")
 border_pen.penup()
 border_pen.setposition(-300,-300)
@@ -16,7 +16,6 @@ for side in range(4):
     border_pen.fd(600)
     border_pen.lt(90)
 border_pen.hideturtle()
-
 #Create the player turtle
 player = turtle.Turtle()
 player.color("blue")
