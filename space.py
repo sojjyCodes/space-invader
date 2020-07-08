0a101a9dfec33f6773d3c3c9a8f3bfd6ef47723d
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-07-08 18:02:46 +0100

    added some enemies and changed the player character

--- space.py
+++ space.py
@@ -19,14 +19,34 @@ border_pen.hideturtle()
 #Create the player turtle
 player = turtle.Turtle()
 player.color("blue")
-player.shape("triangle")
+player.shape("turtle")
 player.penup()
 player.setposition(0, -250)
 player.setheading(90)
 player.pendown()
+#Create the enemies
+enemy_1 = turtle.Turtle()
+enemy_1.color("black")
+enemy_1.shape("triangle")
+enemy_1.penup()
+enemy_1.setposition(-180, 280)
+enemy_1.setheading(270)
+enemy_1.pendown()
 
+enemy_2 = turtle.Turtle()
+enemy_2.color("black")
+enemy_2.shape("triangle")
+enemy_2.penup()
+enemy_2.setposition(15, 280)
+enemy_2.setheading(270)
+enemy_2.pendown()
 
+enemy_3 = turtle.Turtle()
+enemy_3.color("black")
+enemy_3.shape("triangle")
+enemy_3.penup()
+enemy_3.setposition(180, 280)
+enemy_3.setheading(270)
+enemy_3.penup()
 
-
-
-delay = input("Press enter to finish")
+delay = input("Press enter to finish") 
