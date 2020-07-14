992cf968391173b6d540825c30ce4cec42fcfed7
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-07-14 13:44:51 +0100

    Added background image

--- space.py
+++ space.py
@@ -1,10 +1,25 @@
 import turtle
-import random
-import os
+
+#Make a function to move the player 
+def move_left():
+    x = player.xcor()
+    x -= playerspeed
+    if x < -280:
+        x = -280
+    player.setx(x)
+
+def move_right():
+    x = player.xcor()
+    x += playerspeed
+    if x > 280:
+        x = 280
+    player.setx(x)
+
 
 wn = turtle.Screen()
 wn.bgcolor("black")
-wn.title("sojjy's Space invaders")
+wn.bgpic("space_invaders_background.gif")
+wn.title("Space Invaders")
 
 #Draw border
 border_pen = turtle.Turtle() 
@@ -31,30 +46,18 @@ player.setheading(90)
 
 playerspeed = 20
 
-#Make a function to move the player 
-def move_left():
-    x = player.xcor()
-    x -= playerspeed
-    if x < -280:
-        x = -280
-    player.setx(x)
-
-def move_right():
-    x = player.xcor()
-    x += playerspeed
-    if x > 280:
-        x = 280
-    player.setx(x)
 
 #Making the player bullet
 bullet = turtle.Turtle()
 bullet.color("yellow")
-bullet.shape("circle")
+bullet.shape("triangle")
 bullet.speed(0)
 bullet.penup()
+bullet.setpos(0, -230)
 bullet.setheading(90)
 bullet.shapesize(0.5, 0.5)
 bullet.pendown()
+
 #Create keyboard movement
 turtle.listen()
 turtle.onkey(move_left, "Left")
@@ -62,26 +65,25 @@ turtle.onkey(move_right, "Right")
 
 
 #Create the enemies
-enemy_1 = turtle.Turtle()
-enemy_1.color("red")
-enemy_1.shape("triangle")
-enemy_1.penup()
-enemy_1.setposition(0, 280)
-enemy_1.setheading(270)
+enemy = turtle.Turtle()
+enemy.color("red")
+enemy.penup()
+enemy.setposition(0, 280)
+enemy.setheading(270)
 
 enemyspeed = 2
 
 
 while True:
     
-    x = enemy_1.xcor()
+    x = enemy.xcor()
     x += enemyspeed
-    enemy_1.setx(x)
+    enemy.setx(x)
 
-    if enemy_1.xcor() > 280:
+    if enemy.xcor() > 280:
         enemyspeed *= -1
 
-    if enemy_1.xcor() < -280:
+    if enemy.xcor() < -280:
         enemyspeed *= -1
     
 
